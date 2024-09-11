1432b6540e8e0748f82a862476a674d5e069771e
Thiago Kersting Puls <thiagokpuls@gmail.com>
2024-09-10 21:38:11 -0300

    files uploading

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { AuthController } from './auth.controller';
 import { UserModule } from 'src/user/user.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthService } from './auth.service';
+import { FileModule } from 'src/file/file.module';
+
 
 @Module({
   imports: [
@@ -12,6 +14,7 @@ import { AuthService } from './auth.service';
     }),
     forwardRef(() => UserModule),
     PrismaModule,
+    FileModule
   ],
   controllers: [AuthController],
   providers: [AuthService],
